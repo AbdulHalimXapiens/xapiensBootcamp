c0fa9ce7ff62c86a272e5b9f1bc75f57b29a7e4b
AbdulHalimXapiens <Abdul.Halim@xapiens.id>
2021-02-17 09:16:16 +0700

    add

--- week5/Day-21/front-end-react/src/_reducers/vendor.reducer.js
+++ week5/Day-21/front-end-react/src/_reducers/vendor.reducer.js
@@ -1,38 +1,38 @@
-const initialState = { anchor: 'left',
-    vendor: [],
-    open: false,
-    id: '',  
-    name: '',
-    nama_produk: '',
-    harga: '',
-    jumlah_stok: ''
- };
-
+const initialState = {
+  anchor: "left",
+  vendor: [],
+  open: false,
+  id: "",
+  nama_klub: "",
+  nama_pemain: "",
+  posisi: "",
+  kelebihan: "",
+};
 
 export function vendor(state = initialState, action) {
-    switch (action.type) {
-        case 'FETECHED_ALL_VENDOR':
-            return {
-            ...state,
-            vendor: action.vendor
-            };
-        case 'VENDOR_DETAIL':
-            return {
-                ...state,
-                id: action.id,  
-                name: action.name,
-                nama_produk: action.nama_produk,
-                harga: action.harga,
-                jumlah_stok: action.jumlah_stok
-            };
-        case "USER_UPDATED":
-            return state;
-        case "HANDLE_ON_CHANGE":
-            return {
-                ...state,
-                [action.props]: action.value
-            };    
-        default:
-            return state
-    }
+  switch (action.type) {
+    case "FETECHED_ALL_VENDOR":
+      return {
+        ...state,
+        vendor: action.vendor,
+      };
+    case "VENDOR_DETAIL":
+      return {
+        ...state,
+        id: action.id,
+        nama_klub: action.nama_klub,
+        nama_pemain: action.nama_pemain,
+        posisi: action.posisi,
+        kelebihan: action.kelebihan,
+      };
+    case "USER_UPDATED":
+      return state;
+    case "HANDLE_ON_CHANGE":
+      return {
+        ...state,
+        [action.props]: action.value,
+      };
+    default:
+      return state;
   }
+}
